351ece0622b0ff80c6567b0c6854c518976bdc46
HamzaHassanIrshad <hamzahassanoff@gmail.com>
2023-03-26 01:20:12 +0530

    going back to localhost url for the server

--- client/src/components/UserImage.jsx
+++ client/src/components/UserImage.jsx
@@ -4,7 +4,7 @@ const UserImage = ({ image, size = "60px" }) => {
   return (
     <Box width={size} height={size}>
       <img
-        src={`https://social-media-mern-server.vercel.app/assets/${image}`}
+        src={`http://localhost:3001/assets/${image}`}
         alt="user"
         style={{ objectFit: "cover", borderRadius: "50%" }}
         width={size}
